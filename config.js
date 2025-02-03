76895923740427304ffd86043a29d390f648e0fa
cabinas-san-juanillo <cabinassanjuanillo@gmail.com>
2025-02-03 16:39:55 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbybAIvMKvZZO0xVw0debIiBUDqkpydcclYD3U76qf19SnaiT8-SHEeDVCzDLwAJdD8J/exec";
+const AS_URL_BASE = "https://script.google.com/macros/s/AKfycbxTx0z8afw1OAUXdUZmxy1SabdJW_Fhj-z0h6dFrFweQvMPuw6VOiUR2nFinU97Arln/exec";
